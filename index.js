ee368ca7764d4bac6b195888048c5ae630e658b2
Carmen Salas <carmensalas0114@gmail.com>
2025-09-19 11:19:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Write and test your solutions here
+/*
+Complete the function so that it returns the following:
+80 or above → "Wear shorts and a t-shirt."
+60–79 → "Wear jeans and a light jacket."
+40–59 → "Wear a sweater and pants."
+Below 40 → "Wear a heavy coat, scarf, and boots."
+*/
+
+const outfitPicker = (temp) => {
+  // ✍️ Write your code here
+};
+
+//Test your function below
